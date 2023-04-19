00326a5681fb6aaeaefd2a16d4233ee7ecf6e09e
LarsClaassen <larsclaas2015@gmail.com>
2023-04-19 16:47:02 +0200

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("JOey hello world");
+        System.out.println("Joey hello world");
+        System.out.println("Lars hello world");
     }
 }
