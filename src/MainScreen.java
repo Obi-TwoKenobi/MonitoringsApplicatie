e07bcde650e0367dd601b76785e3455169d9c852
LarsClaassen <larsclaas2015@gmail.com>
2023-05-12 10:15:55 +0200

    Icons gemaakt alleen nog  styling

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -1,17 +1,30 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import GUI.infrastructuredesign.InfrastructureDesignGUI;
 import GUI.monitoring.MonitoringsGUI;
+import data.DatabaseserverComponent;
+import data.FirewallComponent;
+import data.InfrastructureDesignComponent;
+import data.WebserverComponent;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.ImageIcon;
 
 public class MainScreen extends JPanel implements ActionListener {
     //buttons
     private JButton monitoring;
     private JFrame hoofdscherm;
+    private final int IMAGE_width = 130;
+    private final int IMAGE_height = 80;
     private InfrastructureDesignGUI infrastructureDesignGUI;
+    private ImageIcon icon0;
+    private ImageIcon icon1;
 
     //buttons
     private JButton ontwerpButton;
@@ -32,14 +45,17 @@ public class MainScreen extends JPanel implements ActionListener {
         this.setDoubleBuffered(true);
         this.hoofdscherm = jFrame;
 
-        monitoring = new JButton("Monitoring");
+        this.icon0 = this.getComponentImage(0);
+        this.icon1 = this.getComponentImage(1);
+
+        monitoring = new JButton(icon0);
         monitoring.addActionListener(this);
         Dimension size = monitoring.getPreferredSize();
         monitoring.setBounds(200, 240, size.width, size.height);
         setLayout(null);
         add(monitoring);
 
-        this.ontwerpButton = new JButton("Ontwerpen");
+        this.ontwerpButton = new JButton(icon1);
         infrastructureDesignGUI = new InfrastructureDesignGUI(hoofdscherm);
         ontwerpButton.setBounds(400, 240, size.width, size.height);
         ontwerpButton.addActionListener(this);
@@ -47,6 +63,20 @@ public class MainScreen extends JPanel implements ActionListener {
 
         setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
+    }
+    private ImageIcon getComponentImage(int keuze){
+        String filePath = "";
+        if(keuze == 0) filePath = "src/resources/images/MonitorWaveformFontAwesome.png";
+        if(keuze == 1) filePath = "src/resources/images/DesktopAwesomeFont.png";
+
+        try{
+            BufferedImage img = ImageIO.read(new File(filePath));
+            Image scaledImage = img.getScaledInstance(IMAGE_width, IMAGE_height, Image.SCALE_SMOOTH);
+            return new ImageIcon(scaledImage);
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+            return new ImageIcon();
+        }
     }
     @Override
     public void actionPerformed(ActionEvent e) {
